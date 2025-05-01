d521c112279e1ebb0bea70a77264b6cdaf8c3b30
resyaglamoda <resya glamoda suhendri>
2025-05-01 12:55:39 +0700

    Membuat deklarasi class

--- Privated.cpp
+++ Privated.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
 using namespace std;
+
+class Segitiga {
+    private:
+        int alas = 10;       // ubah sesuai kasus (contoh: 7 -> ganjil, 8 -> genap)
+        int tinggi = 5;
+        float hasilLuas;
+    
