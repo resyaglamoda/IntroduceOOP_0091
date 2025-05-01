dfe4be108fbb0b25819ed19590091f243deacb3e
resyaglamoda <resya glamoda suhendri>
2025-05-01 12:57:05 +0700

    Membuat fungsi privated hanya dipanggil jika alas genap

--- Privated.cpp
+++ Privated.cpp
@@ -6,4 +6,8 @@ class Segitiga {
         int alas = 10;       // ubah sesuai kasus (contoh: 7 -> ganjil, 8 -> genap)
         int tinggi = 5;
         float hasilLuas;
-    
+     
+    // Fungsi private hanya dipanggil jika alas genap
+    float hitungLuas(int a, int t) {
+        return 0.5 * a * t;
+    }
